1c9005167fe4f340ba47b3f5f44b29bbf4577f0d
Pedro Andriotti <phab@cesar.school>
2023-10-07 22:00:40 -0300

    DAO ok

--- src/br/gov/cesarschool/poo/bonusvendas/dao/CaixaDeBonusDAO.java
+++ src/br/gov/cesarschool/poo/bonusvendas/dao/CaixaDeBonusDAO.java
@@ -1,5 +1,18 @@
 package br.gov.cesarschool.poo.bonusvendas.dao;
 
-public class CaixaDeBonusDAO {
+import java.util.List;
+import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
+import br.gov.cesarschool.poo.bonusvendas.entidade.CaixaDeBonus;
 
+public class CaixaDeBonusDAO{
+        // Usando a Cadastro.Objetos.class
+        private CadastroObjetos cadastroObjetos;
+
+        public CaixaDeBonusDAO(){
+            this.cadastroObjetos = new CadastroObjetos(CaixaDeBonus.class);
+        }
+
+        public void salvarCaixaDeBonus(CaixaDeBonus caixaDeBonus, String chave){
+            cadastroObjetos.incluir(caixaDeBonus, chave);   
+        }
 }
--- src/br/gov/cesarschool/poo/bonusvendas/dao/VendedorDAO.java
+++ src/br/gov/cesarschool/poo/bonusvendas/dao/VendedorDAO.java
@@ -1,5 +1,43 @@
 package br.gov.cesarschool.poo.bonusvendas.dao;
+import java.io.Serializable;
+
+import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
+import   br.gov.cesarschool.poo.bonusvendas.entidade.Vendedor;
+
 
 public class VendedorDAO {
+    private CadastroObjetos cadastro = new CadastroObjetos(Vendedor.class); 
 
+    public boolean incluir(Vendedor vendedor) {
+        Vendedor vendedorBusca = buscar(vendedor.getCpf()); 
+        if (vendedorBusca != null) { 
+            return false;
+        } else {
+            cadastro.incluir(vendedor, vendedor.getCpf());
+            return true;
+        }         
+    }
+
+    public boolean alterar(Vendedor vendedor) {
+        Vendedor vendedorBusca = buscar(vendedor.getCpf());
+        if (vendedorBusca == null) {
+            return false;
+        } else {
+            cadastro.alterar(vendedor, vendedor.getCpf());
+            return true;
+        }        
+    }
+
+    public Vendedor buscar(String cpf) {
+        return (Vendedor) cadastro.buscar(cpf);
+    }
+
+    public Vendedor[] buscarTodos() {
+        Serializable[] rets = cadastro.buscarTodos(Vendedor.class);
+        Vendedor[] vendedores = new Vendedor[rets.length];
+        for(int i=0; i<rets.length; i++) {
+            vendedores[i] = (Vendedor)rets[i];
+        }
+        return vendedores;
+    } 
 }
